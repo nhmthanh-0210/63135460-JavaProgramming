7db9ebbcd42de2fc42565e61686a5e18c804450a
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-01-22 17:06:20 +0700

    Update ex2

--- exJava/src/exJava/bai2.java
+++ exJava/src/exJava/bai2.java
@@ -14,9 +14,13 @@ public class bai2 {
 		System.out.println("Nhap vao chieu rong");
 		double chieuRong = nhap.nextDouble();
 		
+		double min = Math.min(chieuDai, chieuRong);
 		
 		System.out.printf("Chu vi hinh chu nhat la: %f", (chieuDai + chieuRong) * 2);
-		
+		System.out.println();
+		System.out.printf("Dien tich hinh chu nhat la: %f", chieuDai * chieuRong);
+		System.out.println();
+		System.out.printf("Cạnh nhỏ của hình chữ nhật là: %f", min);
 	}
 
 }
