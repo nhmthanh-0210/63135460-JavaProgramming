978ec54b629b3fe22a7e18453a274a3a0be9dbd1
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-01-23 17:00:18 +0700

    Create lab2 bai1

---
+++ exJava/src/lab2/bai1.java
@@ -0,0 +1,37 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+		Scanner nhap = new Scanner(System.in);
+		
+		double a = nhap.nextDouble();
+		double b = nhap.nextDouble();
+		
+		double x = -b/a;
+		
+		
+		
+		if(a == 0){
+			if(b == 0){
+			  System.out.println("Vô số nghiệm");
+			}
+			else{
+				System.out.println("Vô nghiệm");
+			}
+			
+		}
+			else{
+				System.out.printf("kết quả của phương trình là: %.1f", x );
+			}
+			
+			
+			
+		}
+
+}
