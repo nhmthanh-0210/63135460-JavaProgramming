cf0a2d1e58e408ef2e673f7fb358b2eef75f97f2
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-02-18 11:03:24 +0700

    Update bai1.java

--- exJava/src/lab3/bai1.java
+++ exJava/src/lab3/bai1.java
@@ -1,10 +1,35 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class bai1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("thanh");
+			
+		Scanner nhap = new Scanner(System.in);
+		
+		System.out.println("Nhap so nguyen tu ban phim");
+		int n = nhap.nextInt();
+		
+		boolean ok = true;
+		for(int i=2; i < n-1; i++){ 
+		if(n % i == 0){
+		ok = false;
+		break;
+		}
+		 i++;
+		}
+		
+		
+		 if (ok && n > 1) {
+	            System.out.println(n + " là số nguyên tố.");
+	        } else {
+	            System.out.println(n + " không phải là số nguyên tố.");
+	        }
+		
+		
+		
 	}
 
 }
