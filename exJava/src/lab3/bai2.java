c428aa29f8b1ee7a3f07d7b5639ef4fcc7bb2a8a
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-02-25 11:53:21 +0700

    Update bai2.java

--- exJava/src/lab3/bai2.java
+++ exJava/src/lab3/bai2.java
@@ -3,8 +3,14 @@ package lab3;
 public class bai2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		for (int i = 1; i <= 9; i++) {
+            System.out.println("Bảng cửu chương của " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+            System.out.println();
+        }
 	}
 
 }
