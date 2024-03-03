a833b5c255365b883cccb26bf4902ff3a549ce97
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-03-04 05:05:11 +0700

    Update bai1.java

--- exJava/src/lab6/bai1.java
+++ exJava/src/lab6/bai1.java
@@ -1,5 +1,29 @@
 package lab6;
 
-public class bai1 {
+import java.util.Scanner;
 
+public class bai1 {
+	 public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Nhập họ và tên: ");
+	        String hoTen = scanner.nextLine();
+
+	        int viTriKyTuTrangDau = hoTen.indexOf(" ");
+	        int viTriKyTuTrangCuoi = hoTen.lastIndexOf(" ");
+
+	        if (viTriKyTuTrangDau != -1 && viTriKyTuTrangCuoi != -1 && viTriKyTuTrangDau != viTriKyTuTrangCuoi) {
+	            String ho = hoTen.substring(0, viTriKyTuTrangDau).toUpperCase();
+	            String ten = hoTen.substring(viTriKyTuTrangCuoi + 1).toUpperCase();
+	            String tenDem = hoTen.substring(viTriKyTuTrangDau + 1, viTriKyTuTrangCuoi);
+
+	            System.out.println("Họ: " + ho);
+	            System.out.println("Tên đệm: " + tenDem);
+	            System.out.println("Tên: " + ten);
+	        } else {
+	            System.out.println("Họ và tên không hợp lệ.");
+	        }
+
+	  
+	    }
 }
